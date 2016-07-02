7803c1e2cf10a411585491ca0c51e1276ca51e5b
J.Son <x@x-wx.com>
2016-07-02 22:16:12 +0800

    加上排列组合算法

--- countGraphical.js
+++ countGraphical.js
@@ -7,9 +7,14 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 let points = [];
+let num = 3;
+// rl.question(`请输入要计算几边形 (格式: 3 以上数字, 默认3):`, function (_num) {
+//   num = _num || num;
+//   pointsInput();
+// });
 // question方法
-function pointsInput() {
-  rl.question(`请输入第${points.length + 1}条线段中的点${points.length == 0 
+(function pointsInput() {
+  rl.question(`请输入第${points.length + 1}条线段中的点${points.length == 0
   && ' (格式: A, B, F 或者 ABF)' || ' (结束请输入: end)'}：`, function (pts) {
     // 不加close，则不会结束
     if (pts == 'end') {
@@ -21,15 +26,39 @@ function pointsInput() {
       pointsInput();
     }
   });
-}
-pointsInput();
+})();
 // close事件监听
 rl.on("close", function () {
   // 结束程序
-  const result = count(points);
+  const result = count(points, num);
   console.log(`有线段：${result.totalLines.length}条,三角形有${result.triangles.length}`);
   process.exit(0);
 });
+
+
+//组合
+function combination(arr, num) {
+  var r = [];
+  (function f(t, a, n) {
+    if (n == 0) return r.push(t);
+    for (var i = 0, l = a.length; i <= l - n; i++) {
+      f(t.concat(a[i]), a.slice(i + 1), n - 1);
+    }
+  })([], arr, num);
+  return r;
+}
+
+//排列
+function permutation(arr, num) {
+  var r = [];
+  (function f(t, a, n) {
+    if (n == 0) return r.push(t);
+    for (var i = 0, l = a.length; i < l; i++) {
+      f(t.concat(a[i]), a.slice(0, i).concat(a.slice(i + 1)), n - 1);
+    }
+  })([], arr, num);
+  return r;
+}
 
 
 // 数组去重
@@ -59,22 +88,8 @@ Array.prototype.unique = function () {
  [ 'G', 'F' ]
  ]
  */
-function getLines() {
-  let points = arguments;
-  let lines = [];
-  for (let i = 0; i < points.length; i++) {
-    for (let j = i + 1; j < points.length; j++) {
-      if (points[i] == points[j]) {
-        continue;
-      }
 
-      lines.push([points[i], points[j]]);
-    }
-  }
-  return lines;
-}
-
-// 当3个点中,有一个在存在线段中,就能组成三角形
+// 避免3点一线的现象存在
 function isVerify(ary, ary2) {
   let inline = 0;
   ary.map(v => {
@@ -86,42 +101,40 @@ function isVerify(ary, ary2) {
 
 }
 
-const count = (_points) => {
+const count = (_points, num) => {
   let points = _points || [
-      ["A", "B", "F"],
-      ["A", "G", "C", "H"],
-      ["A", "D", "I", "J"],
-      ["A", "E", "K"],
-      ["B", "C", "D", "E"],
-      ["F", "H", "J", "K"],
-      ["E", "I", "G", "F"]
-    ];
+    ["A", "B", "F"],
+    ["A", "G", "C", "H"],
+    ["A", "D", "I", "J"],
+    ["A", "E", "K"],
+    ["B", "C", "D", "E"],
+    ["F", "H", "J", "K"],
+    ["E", "I", "G", "F"]
+  ];
   let totalLines = [];
   points.map(v => {
-    totalLines = totalLines.concat(getLines.apply(null, v));
+    totalLines = totalLines.concat(combination.call(null, v, 2));
   });
   let triangles = [];
-  for (let i = 0; i < totalLines.length; i++) {
-    for (let j = i + 1; j < totalLines.length; j++) {
-      for (let z = j + 1; z < totalLines.length; z++) {
-        let lines = totalLines[i].concat(totalLines[j]).concat(totalLines[z]);
-        // 任意三条线段组合, lines里的点做去重, 算出三条线段一共有多少个点, 当线段组合有且只由3个点组成, 旧能组成一个三角形
-        let geometric = Array.prototype.unique.call(lines);
-        if (geometric.length == 3) {
-          // 判断当前3个点组成的是三角形还是3点一线的线段
-          let isTriangle = true;
-          points.map(v => {
-            if (!isVerify(geometric, v)) {
-              isTriangle = false
-            }
-          });
-          if (!isTriangle) continue;
-          console.log(geometric);
-          triangles.push(geometric);
+  const lines = combination.call(null, totalLines, num);
+  lines.map(v => {
+    let geometric = Array.prototype.unique.call(v);
+    if (geometric.length == num) {
+      // 判断当前3个点组成的是三角形还是3点一线的线段
+      let isTriangle = true;
+      points.map(v => {
+        if (!isVerify(geometric, v)) {
+          isTriangle = false
         }
+      });
+      if (isTriangle) {
+        // console.log(v);
+        console.log(geometric);
+        triangles.push(geometric);
       }
     }
-  }
+  });
+
   return {
     triangles,
     totalLines
